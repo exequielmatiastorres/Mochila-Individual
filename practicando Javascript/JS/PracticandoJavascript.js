17460b2c438cb353f4fbbc8f75a872f09322c48e
exequielmatiastorres <exequieltorres0109@gmail.com>
2023-01-15 18:24:44 -0300

    subiendo archivos de js

---
+++ practicando Javascript/JS/PracticandoJavascript.js
@@ -0,0 +1,74 @@
+console.log("this is a string")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*¿que es javascript?: Es un lenguaje de programación
+Javascript puede:
+*Alterar el documento html
+*cambiar lo que necesitemos
+*detectar dispositivos que se conectan a mi web
+*Animaciones
+*Crear aplicaciones dentro del navegador
+*Crear aplicaciones multiplataforma
+
+
+
+
+tipos de datos:
+
+"" (string)
+'' (string)
+
+console.log("this is a string")
+
+100000 number
+-2.3 number
+
+true (boolean)
+flase (boolean)
+
+array (puedes crear lista de lo que necesites)
+['Joe', 'ryan', 'martha']
+[1, 2, 3, 4]
+[true, false, true, false]
+
+
+objeto (representacion abstracta de un objeto en la vida real) (tipos de datos distintos respecto a un usuario en este caso gochi)
+usuario Gochi:
+'ryan'
+70.3
+14
+true
+
+los objetos se crean con llaves
+
+(ejemplo de objeto relacionado a un solo usuario){
+    "username": 'ryan', (dentro de este objeto de ejemplo que esta relacionado a un solo usuario se les da nombres que se encierran entre comillas pero no son strings: correlacion: clave > valor, puede crear cuantas propiedades vos desees)
+    "score": 70,4,
+    "hours": 14,
+    "professional": true
+}
+
+*/
